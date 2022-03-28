71458f72307c71ed160fc6799b4bb461b5a26240
meaisha <meaisha@gmail.com>
2022-03-28 18:40:01 -0400

    creates json db and displays posts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+
+// component imports
+import PostList from '../components/PostList.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'Home',
+  components: { PostList },
+  setup() { 
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok) {
+          throw Error('no available data')
+        }
+        posts.value = await data.json()
+        console.log(posts.value)
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    load()
+    
+    return { posts, error }
+  },
 }
 </script>
